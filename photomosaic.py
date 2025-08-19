200df102b28d61f7067d2121355ed5910e219553
koriol <oriol.k@gmail.com>
2025-08-19 12:47:10 -0400

    up to page 124. Need to comment 'creating the photomosaic' section

--- photomosaic.py
+++ photomosaic.py
@@ -81,25 +81,91 @@ def getBestMatchIndex(input_avg, avgs):
 
     # input image average
     avg = input_avg
-    #get the closest RGB value to input, based on RGB distance
+    # get the closest RGB value to input, based on RGB distance
+    # initialize the closest match index to 0
     index = 0
+    # minimum distance to infinity
     min_index = 0
     min_dist = float("inf")
-    for cal in avgs:
+    # loop through the values in the list of averages and compute the
+    # distances using distance formula minus the square root to save computational time
+    for val in avgs:
         dis = ((val[0] - avg[0])*(val[0] - avg[0]) +
                (val[1] - avg[1])*(val[1] - avg[1]) +
                (val[2] - avg[2])*(val[2] - avg[2]))
-    if dist < min_dist:
-        min_dist = min_dist
-        min_index = index
-    index += 1
+        # if the computed distance is less than the stored minimum distance min_dist, it's replaced with the new minimum distance
+        if dis < min_dist:
+            min_dist = dis
+            # min_index is the index of the average RGB value from the avgs list that is closest to input_avg
+            min_index = index
+        index += 1
+    return min_index
 
-return min_index
-
+# qavgs is the list of average RGB values for each tile in the target image, and kdtree is the scipy KDTree object created using a list of average RGB values from the input images.
 def getBestMatchIndiciesKDT(qavgs, kdtree):
     """
     return indices of best Image matches based on RGB value distance uses a k-d tree
     """
     # e.g., [array([2.]), array([9], dtype=int64)]
+    # use the KDTree object's query() method to get the points in the tree that are closes to the ones in qavgs. k is the number of nearest neighbors to the queried point you want to return.
+    # k=1 means we want the closest match. The return is a tuple consisting of two numpy arrays with the distances and inndices of the matches. You need indices, so pick the second value from the result.
     res = list(kdtree.query(qavgs, k=1))
     min_indicies = res[1]
+    return min_indicies
+
+# takes two parameters: list of input images you chose based on closest RGB match to the individual tiles of target image, and tuple on photomosaics dimensions (m, n).
+def createImageGrid(images, dims):
+    """
+    given a list of images and a grid size (m, n), create a grid of images
+    """
+    m, n = dims
+    # sanity check: see whether the number of images supplied matches the grid size
+    assert m*n == lin(images)
+
+    # get the maximum height and width of the images
+    # don't assume they're all equal
+    # set max width and height to the maximum of all images for a standard size
+    # if image is too small, solid black will fill the space
+    width = max([img.size[0] for img in images])
+    height = max([img.sizw[1] for img in images])
+
+    # create the target image to fit all images in grid
+    grid_img = Image.new('RGB', (n*width, m*height))
+
+    # paste the tile images into the image grid
+    for index in range (len(images)):
+        row = int(index/n)
+        col = index - n*row
+        # paste the appropriate image into the grid at the right position
+        # the first argument is the image object to be pasted, the second
+        # (col*width, row*height) is the upper-left corner of the tile
+        grid_img.paste(images[index], (col*width, row*height))
+
+    return grid_img
+
+def createPhotomosaic(target_image, input_images, grid_size, reuse_images, use_kdt):
+    """
+    creates photomosaic given target and input images
+    """
+
+    print('splitting input image...')
+    # split target image
+    target_images = splitImage(target_image, grid_size)
+
+    print('finding image matches...')
+    # for each target image, pick one from input
+    output_images = {}
+    # for user feedback
+    count = 0
+    batch_size = int(len(target_images)/10)
+
+    # calculate input image averages
+    avgs = []
+    for img in input_images:
+        avgs.append(getAverageRGB(img))
+
+    # compute target averages
+    avgs_target = []
+    for img in target_images:
+        # target subimage average
+        avgs_target.append(getAverageRGB(img))
